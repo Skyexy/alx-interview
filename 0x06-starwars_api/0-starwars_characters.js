579575f98786c99dae210fea650d0e86ed785997
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-14 21:02:29 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-const me = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/films/${me}/';
-request(url, function (error, response, body) {
+request('https://swapi-api.alx-tools.com/films/'+ process.argv[2], function (error, response, body) {
   if (error) throw error;
   for (const character of JSON.parse(body).characters) {
     request(character, function (error, response, body) {
