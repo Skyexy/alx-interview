3729cf120319c378ac0ea15bdeba11bbccc4f623
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-15 20:35:35 +0100

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,11 +2,12 @@
 
 def rotate_2d_matrix(matrix):
     """ rotatae a 2d matrix 90 degrees clockwise."""
-    sem = [[matrix[j][i] for i in range(len(matrix))] for j in range(len(matrix))]
-    height = len(matrix) - 1
-    for i in range(len(matrix)):
+    lent = len(matrix)
+    sem = [[matrix[j][i] for i in range(lent)] for j in range(lent)]
+    height = lent - 1
+    for i in range(lent):
         width = 0
-        for x in range(len(matrix)):
+        for x in range(lent)):
             matrix[x][i] = sem[height][width]
             width = width + 1
         height = height - 1
